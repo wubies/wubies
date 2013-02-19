40ad22474183681a1cfc0b563402ab046da63293
Julian Smith <wubies@www.wubies.org>
2013-02-19 16:41:40 +1100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wubies::Application.config.secret_token = 'befc98cd8003e287e162dc293a5a5b65f545cc8a28d4657bc59886218a89d834fe468d56db2a58cb41b427730fa8b1550dfa2269f0077c8c46ff72df9a59c7b6'
